3f6c209ee7720b471087b793afe63beb79e5374a
Zack Kourouma <zack@secondspectrum.com>
2017-06-08 07:51:22 -0700

    pattern matching function definition instead of case statement

--- exercism/haskell/space-age/src/SpaceAge.hs
+++ exercism/haskell/space-age/src/SpaceAge.hs
@@ -10,16 +10,14 @@ data Planet = Mercury
             | Neptune
 
 factor :: Planet -> Float
-factor planet =
-  case planet of
-    Mercury -> 0.2408467
-    Venus -> 0.61519726
-    Earth -> 1.0
-    Mars -> 1.8808158
-    Jupiter -> 11.862615
-    Saturn -> 29.447498
-    Uranus -> 84.016846
-    Neptune -> 164.79132
+factor Mercury = 0.2408467
+factor Venus = 0.61519726
+factor Earth = 1.0
+factor Mars = 1.8808158
+factor Jupiter = 11.862615
+factor Saturn = 29.447498
+factor Uranus = 84.016846
+factor Neptune = 164.79132
 
 ageOn :: Planet -> Float -> Float
 ageOn planet seconds = seconds / 31557600 / factor planet

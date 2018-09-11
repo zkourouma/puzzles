a3a2dc800caeb7fe9ba7ed6a25d03c357531fb61
Zack Kourouma <zack@kourouma.me>
2018-09-11 09:30:40 -0700

    Improve variable naming for Pangram

--- exercism/haskell/pangram/src/Pangram.hs
+++ exercism/haskell/pangram/src/Pangram.hs
@@ -5,5 +5,5 @@ module Pangram
 isPangram :: String -> Bool
 isPangram text = null left
   where
-    includes (c, cs) = c `elem` text || cs `elem` text
+    includes (upper, lower) = upper `elem` text || lower `elem` text
     left = dropWhile includes $ zip ['A' .. 'Z'] ['a' .. 'z']

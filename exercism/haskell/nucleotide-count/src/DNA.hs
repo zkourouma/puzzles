90f20ab1d321664354c8a39e83e64320fb4314b1
Zack Kourouma <zack@kourouma.me>
2018-09-26 09:01:47 -0700

    Exercism haskell: nucleotide count

---
+++ exercism/haskell/nucleotide-count/src/DNA.hs
@@ -0,0 +1,18 @@
+module DNA
+  ( nucleotideCounts
+  ) where
+
+import           Data.List (foldl)
+import           Data.Map  (Map, adjust, fromList, member)
+
+basePairs = fromList [('A', 0), ('C', 0), ('T', 0), ('G', 0)]
+
+nucleotideCounts :: String -> Either String (Map Char Int)
+nucleotideCounts = foldl nucleotideCounts' (Right basePairs)
+
+nucleotideCounts' ::
+     Either String (Map Char Int) -> Char -> Either String (Map Char Int)
+nucleotideCounts' (Right acc) c
+  | member c acc = Right (adjust (+ 1) c acc)
+  | otherwise = Left "Error"
+nucleotideCounts' (Left msg) _ = Left msg

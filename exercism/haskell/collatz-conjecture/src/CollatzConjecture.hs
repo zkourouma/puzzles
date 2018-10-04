ae2a0d630793bd9ddd5842d021eb5c0f4ad9ede9
Zack Kourouma <zack@kourouma.me>
2018-10-04 16:48:25 -0700

    Exercism haskell: iterative collatz conjecture

--- exercism/haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ exercism/haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -2,13 +2,18 @@ module CollatzConjecture
   ( collatz
   ) where
 
+import           Data.List
+
 collatz :: Integer -> Maybe Integer
 collatz n
   | n < 1 = Nothing
-  | otherwise = collatz' n (Just 0)
-
-collatz' :: Integer -> Maybe Integer -> Maybe Integer
-collatz' n maybeCount
-  | n == 1 = maybeCount
-  | even n = collatz' (n `div` 2) (fmap (+ 1) maybeCount)
-  | odd n = collatz' (3 * n + 1) (fmap (+ 1) maybeCount)
+  | otherwise =
+    Just $
+    genericLength $
+    takeWhile (/= 1) $
+    iterate
+      (\n ->
+         if even n
+           then n `div` 2
+           else 3 * n + 1)
+      n

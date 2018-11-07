8f472e278edba95181c3645c83b242e7874b902a
Zack Kourouma <zack@kourouma.me>
2018-11-07 09:16:30 -0800

    Exercism haskell: grade school

---
+++ exercism/haskell/grade-school/src/School.hs
@@ -0,0 +1,34 @@
+module School
+  ( School
+  , add
+  , empty
+  , grade
+  , sorted
+  ) where
+
+import           Data.List (find, insert, sort, sortBy)
+
+type School = [(Int, [String])]
+
+add :: Int -> String -> School -> School
+add gradeNum student school =
+  (gradeNum, insert student students) : filter (\g -> fst g /= gradeNum) school
+  where
+    students = grade gradeNum school
+
+empty :: School
+empty = []
+
+grade :: Int -> School -> [String]
+grade gradeNum school =
+  case grade' gradeNum school of
+    Nothing -> []
+    Just g  -> snd g
+
+grade' :: Int -> School -> Maybe (Int, [String])
+grade' gradeNum = find byGrade
+  where
+    byGrade g = fst g == gradeNum
+
+sorted :: School -> [(Int, [String])]
+sorted school = sort $ map (\(g, names) -> (g, sort names)) school

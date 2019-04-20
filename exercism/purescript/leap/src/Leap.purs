25954af2d06f20a9647dc30c9b14078b09d8dc6e
Zack Kourouma <zack@kourouma.me>
2019-04-19 20:23:25 -0700

    Exercism purescript slight refactor

--- exercism/purescript/leap/src/Leap.purs
+++ exercism/purescript/leap/src/Leap.purs
@@ -3,7 +3,7 @@ module Leap where
 import Prelude
 
 isLeapYear :: Int -> Boolean
-isLeapYear n = n `isModulo` 4 && (n `mod` 100 /= 0 || n `isModulo` 400) 
+isLeapYear n = n `isModulo` 4 && (not isModulo n 100 || n `isModulo` 400)
 
 isModulo :: Int -> Int -> Boolean
 isModulo x y = mod x y == 0

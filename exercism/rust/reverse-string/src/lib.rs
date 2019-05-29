cfdb8ff2215889226e8d349efa331c2d87bdb927
Zack Kourouma <zack@kourouma.me>
2019-05-29 09:47:30 -0700

    Exercism: Rust reverse-string handles grapheme clusters

--- exercism/rust/reverse-string/src/lib.rs
+++ exercism/rust/reverse-string/src/lib.rs
@@ -1,3 +1,6 @@
+extern crate unicode_segmentation;
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    input.chars().rev().collect()
+    UnicodeSegmentation::graphemes(input, true).rev().collect()
 }

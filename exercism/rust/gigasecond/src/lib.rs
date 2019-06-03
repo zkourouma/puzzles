971966637a6c4bbf274100c2700d173fe023425d
Zack Kourouma <zack@kourouma.me>
2019-06-03 08:56:31 -0700

    Store number for gigaseconds in a constant

--- exercism/rust/gigasecond/src/lib.rs
+++ exercism/rust/gigasecond/src/lib.rs
@@ -1,7 +1,9 @@
 use chrono::{DateTime, Utc};
 use time::Duration;
 
+const ONE_BILLION: i64 = 1_000_000_000;
+
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start + Duration::seconds(1_000_000_000)
+    start + Duration::seconds(ONE_BILLION)
 }
